8cc434bbdb6fe54e1b39676e2fc6d7cda2154bf3
unknown <alpatovdanila@gmail.com>
2020-07-13 22:51:29 +0300

    Updates

---
+++ precache-manifest.8a0a9862440390be8708ece1b51b6a99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bab9e78214cf67e1ddaabe7d21ce6f",
+    "url": "/codix-test-task/index.html"
+  },
+  {
+    "revision": "b446da5452f2ba2c5f5d",
+    "url": "/codix-test-task/static/css/main.d2384630.chunk.css"
+  },
+  {
+    "revision": "db7401d0bf9808159761",
+    "url": "/codix-test-task/static/js/2.bc6f0b27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/codix-test-task/static/js/2.bc6f0b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b446da5452f2ba2c5f5d",
+    "url": "/codix-test-task/static/js/main.6eea8fa7.chunk.js"
+  },
+  {
+    "revision": "f533eef42c76bf426a00",
+    "url": "/codix-test-task/static/js/runtime-main.00be2119.js"
+  },
+  {
+    "revision": "f2b1e5e7f0815eab1eb0e135dd0c7a5f",
+    "url": "/codix-test-task/static/media/DUCK.f2b1e5e7.png"
+  },
+  {
+    "revision": "b9a24d8b0b69e9ba955b3590478da337",
+    "url": "/codix-test-task/static/media/chevron-right.b9a24d8b.svg"
+  },
+  {
+    "revision": "f9e0816864dd9b56fd54b4559c83b2d3",
+    "url": "/codix-test-task/static/media/triangle-down.f9e08168.svg"
+  }
+]);
